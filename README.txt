7e32607704377c08cb069cee56593451c420a71f
SamuelSebastianMartin <sm2@soas.ac.uk>
2020-06-02 23:55:31 +0100

    updated documentation

--- README.txt
+++ README.txt
@@ -1,9 +1,56 @@
-The Python3 files in this folder are for preparing the report data before the mail-merge. The output is an excel spreadsheet with all the necessary data in it.
+Report Collator
+===============
 
-For QM students and UMW students there are different programs (can you use *Qm.py for both?)
+The file report_merge.py in this folder is for preparing the report data
+before the mail-merge. The output is an excel spreadsheet with all the
+necessary data in it.
 
-Not listed on github are:
-  * the `.csv` files downloaded from Moodle. These provide the grades, and should be unchanged from their downloaded format.
-  * the `csv` files containing the names of students. These should have 3 columns, named Email address,Surname,Forenames. Note that "Email address" must be written exactly (Capital E, no white space). The first row is the column headings, the remaining rows are students.
+Use
+---
+
+  1. Ensure data files are in the directory:
+        acengmarks.csv, umwmarks.csv (qmmarks.csv if needed)
+        plus the names-emails csv
+
+  2. `python3 report_merge.py`
+
+Data files
+----------
+
+  * the `.csv` files downloaded from Moodle. These provide the grades,
+    and should be unchanged from their downloaded format.
+    They must be named:
+        ```
+        acengmarks.csv, umwmarks.csv (qmmarks.csv if needed)
+        ```
+
+  * the `csv` files containing the names of students. These should have
+    3 columns, named Email address,Surname,Forenames.
+    Note that "Email address" must be written exactly (Capital E,
+    no white space). The first row is the column headings, the
+    remaining rows are students. For example:
+        ```
+        Email address,forename,surname
+        654732@soas.ac.uk,John,Smith
+        etc.
+        ```
 
 Note to self: Please leave a copy of last terms .csv files in the folder as an aide mémoire for the correct filenames - data files must be named correctly.
+
+
+STILL TO DO
+-----------
+
+  * allow option of what dbs to combine - umw, eng (+qm - optional)
+
+  * capitalise surname, and Camel  first-name
+
+  * change umw, eng, qm filenames to moodleqm.csv, moodleumw.csv etc.
+
+  * visual file-picker for opening
+
+  * more forgiving and fuzzy recognition of email column.
+
+  * display list of columns - click to select/reject (or some way to
+    deal with the long string-literals of column values.
+
--- report_merge.py
+++ report_merge.py
@@ -1,27 +1,28 @@
-# filename: reportmerge.py
+#! /usr/bin/env python3
 
-## to merge 4 csv files useful for report writing.
-## May 2018
-## All files must be .csv
-## names file must be one name per line
-## other files must be as exported by Moodle.
-## dbs are merged on 'Email address' - must be exact
 
-# STILL TO DO
+"""
+This is a program to merge csv files into a single Excel spreadsheet.
+It requires 2 input file types:
+  - a csv file downloaded from moodle gradebook for each of
+    academic English, UMW and (optionally) QM.
+    - a csv list of student emails & names. The format must be:
+        Email address,forename,surname
+        654732@soas.ac.uk,John,Smith
+        etc.
 
-# allow option of what dbs to combine - umw, eng (+qm - optional)
-# capitalise surname, and Camel  firstname
-# change umw, eng, qm filenames to moodleqm.csv, moodleumw.csv etc.
-# visual file-picker for opening
-# more forgiving and fuzzy recognition of email column.
-# display list of columns - click to select/reject (or some way to deal with the long
-#      string-literals of column.values. - maybe using column numbers.
+Dataframes are merged on the 'Email address' column.
+
+The final Excel .xlxs file will be used to mailmerge report forms.
+"""
 
 import os
 from pandas import *
 
+
 def import_data():
     all_csv = os.listdir()
+    # Write out instructions.
     print('This program takes a csv file with a list of student names and "Email address"')
     print('There must be a file from moodle in this directory called acengmarks.csv')
     print('and others called umwmarks.csv and qmmarks.csv')
@@ -31,22 +32,28 @@ def import_data():
         if csv_name.endswith('csv'):
             print(csv_name)
     print()
+
+    # Choose list of student names.
     filename_in = input('What is the name of your <names + email> file: ')
     names = read_csv(filename_in)
     names['Email address'].str.strip() #remove whitespace on key column
 
+    # Load QM marks if file exists.
     if 'qmmarks.csv' in all_csv:
         qm = pandas.read_csv('qmmarks.csv')
         qm['Email address'].str.strip()
         # drop unwanted columns
 #        qm = qm.drop(['First name', 'Surname', 'ID number', 'Institution', 'Department',], axis=1)
-    else: qm = names
-    
+    else: qm = names  # Placeholder with no new data.
+
+    # Load UMW marks
     umw = read_csv('umwmarks.csv')
     umw['Email address'].str.strip()
 #    umw = umw.drop(['First name', 'Surname', 'ID number', 'Institution', 'Department'
 #                    , 'Course total (Real)', 'Last downloaded from this course'
 #                    , 'Course total (Real)', 'Last downloaded from this course'], axis=1)
+
+    # Load Academic English marks
     eng = read_csv('acengmarks.csv')
     eng['Email address'].str.strip()
 #    eng = eng.drop(['First name', 'Surname', 'ID number', 'Institution', 'Department',
@@ -67,7 +74,6 @@ def merge_dataframes(names, umw, eng, qm):
     temp_merge = pandas.merge(names, eng, how='left', on='Email address')
     temp_merge2 = pandas.merge(temp_merge, umw, how='left', on='Email address')
     merged = pandas.merge(temp_merge2, qm, how='left', on='Email address')
-
     return merged
 
 def rename_columns(df):
